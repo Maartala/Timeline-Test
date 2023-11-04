85ef2736c79796aca60f3bd6d24ad77ce5fcca5b
Maartala <Steffi.Gerstenmaier@t-online.de>
2023-11-04 18:55:00 +0100

    JS added ScrollReveal

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const timelineContent = document.querySelectorAll(".timeline-content");
+    const sr = ScrollReveal()
+
+    function addReveal(classname, origin) {
+        sr.reveal(classname, {
+            origin: origin,
+            distance: "300px",
+            easing: "ease-in-out",
+            duration: 800,
+        });
+    }
+
+    if (window.innerWidth < 768) {
+        timelineContent.forEach(function (content) {
+            if (content.classList.contains("js--fadeInLeft")) {
+                content.classList.remove("js--fadeInLeft");
+                content.classList.add("js--fadeInRight");
+            }
+        });
+        addReveal(".js--fadeInRight", "right")
+    } else {
+        addReveal(".js--fadeInLeft", "left");
+        addReveal(".js--fadeInRight", "right");
+    }
+    addReveal(".js--fadeInLeft", "left");
+    addReveal(".js--fadeInRight", "right");
+})
